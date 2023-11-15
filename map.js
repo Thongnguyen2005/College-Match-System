64896255c677dc90515c6f30c0dd60a10c73c786
arshiya-salehi <arshiya.salehi2013@gmail.com>
2023-11-15 10:22:39 -0800

    changes made for interface

---
+++ map.js
@@ -0,0 +1,10 @@
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "53c1d93462e7e87b",
+      center: { lat: 48.85, lng: 2.35 },
+      zoom: 12,
+    });
+  }
+  
+  window.initMap = initMap;
+
