92d3b5c9297e08edb3158eb7bf8865a9c5d1e732
arshiya-salehi <arshiya.salehi2013@gmail.com>
2023-11-29 16:42:16 -0800

    the goecode is now working just need to make the correct API

--- map.js
+++ map.js
@@ -1,39 +1,31 @@
 function initMap() {
-    new google.maps.Map(document.getElementById("map"), {
-      mapId: "53c1d93462e7e87b",
-      center: { lat: 48.85, lng: 2.35 },
+  new google.maps.Map(document.getElementById("map"), {
+    mapId: "53c1d93462e7e87b",
+    center: { lat: 48.85, lng: 2.35 },
+    zoom: 12,
+  });
+}
+function locateCity(){
+const city = document.getElementById("name").value;
+const geocoder = new google.maps.Geocoder();
+geocoder.geocode({address: city}, (results, status) => {
+  if(status==="OK"){
+    // The map, centered at that city
+    const myMap = new google.maps.Map(document.getElementById("map"), {
+      center: results[0].geometry.location,
       zoom: 12,
     });
-  }
-  
-
-  // find the lat and lng of for the center of the initMap function
-  function locateCity(){
-    const city = document.getElementById("name").value;
-    const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({address: city}, (results, status) => {
-      // check if the lat and lng have been found
-      if(status==="OK"){
-        // The map, centered at that city and replace the previse map
-        const myMap = new google.maps.Map(document.getElementById("map"), {
-          center: results[0].geometry.location,
-          zoom: 12,
-        });
-        // The marker, positioned at that city
-        new google.maps.Marker({
-          position: results[0].geometry.location,
-          map: myMap,
-        });
-  
-        // if not would be give you a alart on the page that it does not found the location
-  }else{
-    alert("Geocode was not successful for the following reason: " + status);
-  }
+    // The marker, positioned at that city
+    new google.maps.Marker({
+      position: results[0].geometry.location,
+      map: myMap,
     });
-  };
 
+}else{
+alert("Geocode was not successful for the following reason: " + status);
+}
+});
+};
 
-  window.initMap = initMap;
+window.initMap = initMap;
 
-
-  // prev key: AIzaSyBgbPNYjLDlYcVMeZuou_6jacLCMshLhFQ&map_ids=53c1d93462e7e87b
